4b6af7f0f9d9ef89ce5510146c7a4f9c52550d5f
Nicolas Long <nicolaslong@gmail.com>
2013-03-30 20:23:33 +0000

    added basic content

--- src/sf2ny/sf2ny.php
+++ src/sf2ny/sf2ny.php
@@ -4,22 +4,27 @@ $app = require __DIR__ . '/bootstrap.php';
 
 // homepage
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('page.twig');
+    return $app['twig']->render('homepage.twig');
 });
 
 // who we are
 $app->get('/who-we-are', function () use ($app) {
-    return 'who we are page';
+    return $app['twig']->render('who-we-are.twig');
+});
+
+// why are we doing it?
+$app->get('/why-are-we-doing-it', function () use ($app) {
+    return $app['twig']->render('why-are-we-doing-it.twig');
 });
 
 // route
 $app->get('/route', function () use ($app) {
-    return 'route page';
+    return $app['twig']->render('route.twig');
 });
 
 // charities
 $app->get('/charities', function () use ($app) {
-    return 'charities page';
+    return $app['twig']->render('charities.twig');
 });
 
 // sponsor us
